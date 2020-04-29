2e287395167b36275d8a34508aeb56ee0212595e
Andrew Woods <andrewwoods88@gmail.com>
2020-04-29 12:54:30 -0400

    built battle and results component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,82 +1,80 @@
-import React, { Component } from 'react'
-import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa';
-import PropTypes from 'prop-types';
-import Results from './Results';
-
+import React from 'react'
+import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import Results from './Results'
 
 function Instructions () {
   return (
-    <div className="instructions-container">
-      <h1 className="center-text header-lg">
+    <div className='instructions-container'>
+      <h1 className='center-text header-lg'>
         Instructions
       </h1>
-
-      <ol className="container-sm grid center-text battle-instructions">
+      <ol className='container-sm grid center-text battle-instructions'>
         <li>
-          <h3 className="header-sm">Enter two Github Users</h3>
-          <FaUserFriends className="bg-light" color='rgb(255,191,116)' size={140} />
+          <h3 className='header-sm'>Enter two Github users</h3>
+          <FaUserFriends className='bg-light' color='rgb(255, 191, 116)' size={140} />
         </li>
         <li>
-          <h3 className="header-sm">Battle</h3>
-          <FaFighterJet className="bg-light" color='#727272' size={140} />
+          <h3 className='header-sm'>Battle</h3>
+          <FaFighterJet className='bg-light' color='#727272' size={140} />
         </li>
         <li>
-          <h3 className="header-sm">See the winners</h3>
-          <FaTrophy className="bg-light" color='rgb(255,215,0)' size={140} />
-        </li>                
+          <h3 className='header-sm'>See the winners</h3>
+          <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140} />
+        </li>
       </ol>
-     
-    </div> 
+    </div>
   )
 }
 
-class PlayerInput extends Component {
+class PlayerInput extends React.Component {
   constructor(props) {
     super(props)
+
     this.state = {
-      username: ''                   
+      username: ''
     }
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
+  handleSubmit(event) {
+    event.preventDefault()
 
-  handleChange(e){
-    let value = e.target.value;
-    this.setState({ username: value})
+    this.props.onSubmit(this.state.username)
   }
-
-  handleSubmit (e) {
-    e.preventDefault()
-    this.props.onSubmit(this.state.username)  
+  handleChange(event) {
+    this.setState({
+      username: event.target.value
+    })
   }
-
-
-  render () {
+  render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="username">{this.props.label}</label>
-        <div className="row player-inputs">
-            <input 
-              onChange={this.handleChange} 
-              value={this.state.username} 
-              type="text"
-              autoComplete='off'
-              placeholder='github username'
-              className="input-light"
-              id='username'
-              />   
-        </div>
-          <button 
-            type="submit"
-            // IF username is falsey (empty), then make disabled TRUE
+      <form className='column player' onSubmit={this.handleSubmit}>
+        <label htmlFor='username' className='player-label'>
+          {this.props.label}
+        </label>
+        <div className='row player-inputs'>
+          <input
+            type='text'
+            id='username'
+            className='input-light'
+            placeholder='github username'
+            autoComplete='off'
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <button
+            className='btn dark-btn'
+            type='submit'
             disabled={!this.state.username}
-            className="btn dark-btn"
-            >Submit me</button>
+          >
+            Submit
+          </button>
+        </div>
       </form>
     )
   }
-
 }
 
 PlayerInput.propTypes = {
@@ -86,18 +84,23 @@ PlayerInput.propTypes = {
 
 function PlayerPreview ({ username, onReset, label }) {
   return (
-    <div className="column player">
-      <h3 className="player-label">{label}</h3>
-      <div className="row bg-light">
-        <div className="player-info">
+    <div className='column player'>
+      <h3 className='player-label'>{label}</h3>
+      <div className='row bg-light'>
+        <div className='player-info'>
           <img
-            className="avatar-small"
-            src={`https://github.com/${username}.png?size=200`} 
-            alt={`Avatar for ${username}`}/>
-          <a href={`https://github.com/${username}`} className='link'>{username}</a>
+            className='avatar-small'
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a
+            href={`https://github.com/${username}`}
+            className='link'>
+              {username}
+          </a>
         </div>
-        <button onClick={onReset} className='link'>
-          <FaTimesCircle color='rgb(19,57,42)' size={26} /> 
+        <button className='btn-clear flex-center' onClick={onReset}>
+          <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
         </button>
       </div>
     </div>
@@ -110,7 +113,7 @@ PlayerPreview.propTypes = {
   label: PropTypes.string.isRequired
 }
 
-export default class Battle extends Component {
+export default class Battle extends React.Component {
   constructor(props) {
     super(props)
 
@@ -120,70 +123,69 @@ export default class Battle extends Component {
       battle: false
     }
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleReset = this.handleReset.bind(this);    
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleReset = this.handleReset.bind(this)
   }
-
-  handleSubmit(id, player){
-    this.setState({ 
+  handleSubmit(id, player) {
+    this.setState({
       [id]: player
     })
   }
-
   handleReset(id) {
     this.setState({
       [id]: null
     })
   }
-
   render() {
+    const { playerOne, playerTwo, battle } = this.state
 
-    const {playerOne, playerTwo, battle } = this.state;
-
-    if ( battle === true) {
-      return <Results playerOne={playerOne} playerTwo={playerTwo}/>
+    if (battle === true) {
+      return <Results playerOne={playerOne} playerTwo={playerTwo} />
     }
 
     return (
-      <>
+      <React.Fragment>
         <Instructions />
-        
-        <div className="players-container">
-          <h1 className="center-text header-lg">Players</h1>
-          <div className="row space-around">
-            {playerOne === null ?  
-              <PlayerInput
-                label='Player One'
-                // going to pass onSubmit whats in the input field - player - which is players name - which comes from the component PlayerInputs' state
-                onSubmit={(player) => this.handleSubmit('playerOne', player) }
-              />
-             : <PlayerPreview username={playerOne} label='playerOne' onReset={() => this.handleReset('playerOne')} />
-          
-          }
-          {playerOne !== null && <h2>{this.state.playerOne}</h2>}
 
+        <div className='players-container'>
+          <h1 className='center-text header-lg'>Players</h1>
+          <div className='row space-around'>
+            {playerOne === null
+              ? <PlayerInput
+                  label='Player One'
+                  onSubmit={(player) => this.handleSubmit('playerOne', player)}
+                />
+              : <PlayerPreview
+                  username={playerOne}
+                  label='Player One'
+                  onReset={() => this.handleReset('playerOne')}
+                />
+            }
 
-            {playerTwo === null ? 
-              <PlayerInput
-                label='Player One'
-                onSubmit={(player) => this.handleSubmit('playerTwo', player)}
-              />
-              : <PlayerPreview username={playerTwo} label='playerTwo' onReset={() => this.handleReset('playerTwo')} />
-            }  
-            {playerTwo !== null && <h2>{this.state.playerTwo}</h2>}          
+            {playerTwo === null
+              ? <PlayerInput
+                  label='Player Two'
+                  onSubmit={(player) => this.handleSubmit('playerTwo', player)}
+                />
+              : <PlayerPreview
+                  username={playerTwo}
+                  label='Player Two'
+                  onReset={() => this.handleReset('playerTwo')}
+                />
+            }
           </div>
 
-          {playerOne && playerTwo && (
-            <button 
-              className="btn btn dark-btn btn-space"
-              onClick={() => this.setState({battle: true})}
-              > Get Results
-            </button> 
 
+          {playerOne && playerTwo && (
+            <button
+              className='btn dark-btn btn-space'
+              onClick={() => this.setState({battle: true})}
+            >
+              Battle
+            </button>
           )}
         </div>
-
-      </>
+      </React.Fragment>
     )
   }
 }
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,116 +1,142 @@
-import React from 'react';
-import { battle } from '../utils/api';
-import { FaUser, FaUsers, FaUserFriends, FaCompass, FaBriefcase } from 'react-icons/fa';
+import React from 'react'
+import { battle } from '../utils/api'
+import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
 
 export default class Results extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state ={
+    this.state = {
       winner: null,
       loser: null,
       error: null,
       loading: true
     }
   }
+  componentDidMount () {
+    const { playerOne, playerTwo } = this.props
 
-  componentDidMount(){
-    const { playerOne, playerTwo } = this.props;
-
-    battle([playerOne, playerTwo])
+    battle([ playerOne, playerTwo ])
       .then((players) => {
-        this.setState({ 
+        this.setState({
           winner: players[0],
           loser: players[1],
           error: null,
-          loading: null
+          loading: false
         })
-        console.log(this.state)
       }).catch(({ message }) => {
-        this.setState({ 
+        this.setState({
           error: message,
           loading: false
         })
       })
   }
+  render() {
+    const { winner, loser, error, loading } = this.state
+    console.log('winner:', winner);
+    console.log('loser', loser);
 
-  render () {
-
-    const { winner, loser, error, loading } = this.state;
-
-    if (loading) {
-      return <p>LOADING...</p>
+    if (loading === true) {
+      return <p>LOADING</p>
     }
 
     if (error) {
       return (
-        <p>{error}</p>
-        )
+        <p className='center-text error'>{error}</p>
+      )
     }
 
     return (
-      <div className="grid space-around container-sm">
-        <div className="card bg-light">
-          <h4 className="header-lg center-text">
+      <div className='grid space-around container-sm'>
+        <div className='card bg-light'>
+          <h4 className='header-lg center-text'>
             {winner.score === loser.score ? 'Tie' : 'Winner'}
           </h4>
-          <img src={winner.profile.avatar_url} alt={winner.profile.login} />
-          <h2 className="center-text">
-            <a href={winner.profile.html_url}>
+          <img
+            className='avatar'
+            src={winner.profile.avatar_url}
+            alt={`Avatar for ${winner.profile.login}`}
+          />
+          <h4 className='center-text'>
+            Score: {winner.score.toLocaleString()}
+          </h4>
+          <h2 className='center-text'>
+            <a className='link' href={winner.profile.html_url}>
               {winner.profile.login}
             </a>
           </h2>
-          <ul className="class-list">
+          <ul className='card-list'>
             <li>
-              <FaUser color='rgb(144,115,255)' size={22} />
+              <FaUser color='rgb(239, 115, 115)' size={22} />
               {winner.profile.name}
             </li>
-
             {winner.profile.location && (
               <li>
-                <FaCompass color='rgb(144,115,255)' size={22}/>
-                { winner.profile.location }
+                <FaCompass color='rgb(144, 115, 255)' size={22} />
+                {winner.profile.location}
               </li>
-              )}
-            
-   
-
+            )}
+            {winner.profile.company && (
+              <li>
+                <FaBriefcase color='#795548' size={22} />
+                {winner.profile.company}
+              </li>
+            )}
             <li>
-                <FaUsers color='rgb(144,115,255)' sie={22} />
-                {winner.profile.followers.toLocaleString()} followers
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {winner.profile.followers.toLocaleString()} followers
             </li>
             <li>
-              <FaUserFriends color='rgb(144,115,255)' sie={22} />
+              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
               {winner.profile.following.toLocaleString()} following
             </li>
           </ul>
-        </div> 
-        <div className="card bg-light">
-          <h4 className="header-lg center-text">
-            {loser.score === winner.score ? 'Tie' : 'loser'}
+        </div>
+        <div className='card bg-light'>
+          <h4 className='header-lg center-text'>
+            {winner.score === loser.score ? 'Tie' : 'Loser'}
           </h4>
-          <img src={loser.profile.avatar_url} alt={loser.profile.login} />
-          <h2 className="center-text">
-            <a href={loser.profile.html_url}>
+          <img
+            className='avatar'
+            src={loser.profile.avatar_url}
+            alt={`Avatar for ${loser.profile.login}`}
+          />
+          <h4 className='center-text'>
+            Score: {loser.score.toLocaleString()}
+          </h4>
+          <h2 className='center-text'>
+            <a className='link' href={loser.profile.html_url}>
               {loser.profile.login}
             </a>
           </h2>
-          <ul className="class-list">
-            <li>
-              <FaUser color='rgb(144,115,255)' size={22} />
+          <ul className='card-list'>
+         { loser.profile.name && ( <li>
+              <FaUser color='rgb(239, 115, 115)' size={22} />
               {loser.profile.name}
             </li>
-
+            )}
+            {loser.profile.location && (
+              <li>
+                <FaCompass color='rgb(144, 115, 255)' size={22} />
+                {loser.profile.location}
+              </li>
+            )}
+            {loser.profile.company && (
+              <li>
+                <FaBriefcase color='#795548' size={22} />
+                {loser.profile.company}
+              </li>
+            )}
             <li>
-                <FaUsers color='rgb(144,115,255)' sie={22} />
-                {loser.profile.followers.toLocaleString()} followers
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {loser.profile.followers.toLocaleString()} followers
             </li>
             <li>
-              <FaUserFriends color='rgb(144,115,255)' sie={22} />
+              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
               {loser.profile.following.toLocaleString()} following
             </li>
           </ul>
-        </div>        
+        </div>
       </div>
     )
   }
